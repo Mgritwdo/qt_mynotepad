f39fc4108a64d09ff9d542d7049af7a6ff5d4c6e
Do <99457186@qq.com>
2024-09-11 17:22:37 +0800

    QT学习：实现文本编辑器功能，包括新建文件、打开文件、编写文件和另存为文件

---
+++ mainwindow.cpp
@@ -0,0 +1,61 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //绑定信号
+    connect(ui->newAction,&QAction::triggered,this,&MainWindow::newActionSlot);
+    connect(ui->openAction,&QAction::triggered,this,&MainWindow::openActionSlot);
+    connect(ui->action_S,&QAction::triggered,this,&MainWindow::saveActionSlot);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::newActionSlot()
+{
+    //清空文本框
+    ui->textEdit->clear();
+
+    //更改标题框
+    this->setWindowTitle("新建文本文档.txt");
+}
+
+void MainWindow::openActionSlot()
+{
+    QString filestr = QFileDialog::getOpenFileName(this,"请选择一个文件",QCoreApplication::applicationFilePath(),"*.txt");
+//    qDebug() << filestr;
+
+    QFile file(filestr);
+    file.open(QIODevice::ReadOnly);
+    QByteArray ba = file.readAll();
+    ui->textEdit->setText(QString(ba));
+}
+
+void MainWindow::saveActionSlot()
+{
+    QString filestr = QFileDialog::getSaveFileName(this,"请选择一个文件",QCoreApplication::applicationFilePath());
+
+    if( filestr.isEmpty())
+    {
+
+    }
+    else
+    {
+        QFile file(filestr);
+        file.open(QIODevice::WriteOnly);
+        QByteArray ba;
+        ba.append(ui->textEdit->toPlainText());
+        file.write(ba);
+        file.close();
+    }
+
+}
